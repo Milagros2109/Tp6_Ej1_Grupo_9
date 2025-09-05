d327a8b48ee84debcec5fea02ee62f0dce456c29
Alfaro <alfaromilagrosg@gmail.com>
2025-09-05 16:59:01 -0300

    Add files via upload

---
+++ src/tp6_ejercicio1_grupo9/Tp6_ejercicio1_grupo9.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp6_ejercicio1_grupo9;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tp6_ejercicio1_grupo9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
